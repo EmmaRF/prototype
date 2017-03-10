9d4c2607ba0bce6522a191ca7800ff46a1c989ac
kmanning <kmanning@mills.edu>
2017-03-10 23:36:05 +0000

    Populated sidebar, set fixed in levels, added icons instead of text links

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -7,6 +7,9 @@ router.get('/', ctrlMain.index);
 router.get('/login', ctrlMain.login);
 router.get('/resources', ctrlMain.resources);
 router.get('/signup', ctrlMain.signup);
-router.get('/sidebar', ctrlMain.sidebar);
+router.get('/game1', ctrlMain.game1);
+router.get('/easy', ctrlMain.easy);
+router.get('/medium', ctrlMain.medium);
+router.get('/hard', ctrlMain.hard);
 
 module.exports = router;
