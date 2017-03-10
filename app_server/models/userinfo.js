d04cf0e6ccafc05661bb715f10aaeb4e4e0420e9
Maris Kanouff <mpicherkanouff@mills.edu>
2017-03-10 19:43:42 +0000

    updated userSchema

--- app_server/models/userinfo.js
+++ app_server/models/userinfo.js
@@ -1,10 +1,16 @@
 var mongoose = require("mongoose");
 
+var detailsSchema = new mongoose.Schema({
+    level: { levelNum: Number, levelName: String },
+    favGames: [String]
+});
+
 var userSchema = new mongoose.Schema({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     email: { type: String, required: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    details: {type: detailsSchema, required: false }
 });
 
 mongoose.model('User', userSchema, 'users');
