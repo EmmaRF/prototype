9f8f0391f3945805d5ebfb566b4c94ad438a6b20
Maris Kanouff <mpicherkanouff@mills.edu>
2017-03-31 21:01:27 +0000

    get and post methods added

--- app_api/controllers/users.js
+++ app_api/controllers/users.js
@@ -7,13 +7,50 @@ var sendJsonResponse = function(res, status, content) {
 };
 
 module.exports.user = function(req, res) { 
-    sendJsonResponse(res, 200, {"status" : "success"})
+    User
+        .find()
+        .exec(function(err, user) {    
+            res
+                .json(user);  // creates json response with the data 
+    });
 };
 module.exports.userCreate = function(req, res) { 
-    sendJsonResponse(res, 200, {"status" : "success"});
+    
+    User.create({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        password: req.body.password,
+        email: req.body.email
+    }, function (err, user) {
+        if (err) {
+            sendJsonResponse(res, 400, err);
+        } else {
+            sendJsonResponse(res, 201, user);
+        }
+    })
 };
 module.exports.userProfile = function(req, res) { 
-    sendJsonResponse(res, 200, {"status" : "success"})
+    if (req.params && req.params.userid) {
+        User
+            .findById(req.params.userid)
+            .exec(function(err, user) {
+                if (!user) {
+                    sendJsonResponse(res, 404, {
+                        "message" : "userid not found"
+                    });
+                    return;
+                } else if (err) {
+                    sendJsonresponse(res, 404, err);
+                    return;
+                }
+                sendJsonresponse(res, 200, user);
+                
+            })
+    } else {
+        sendJsonResponse(res, 404, {
+            "message" : "No userid in request"
+        });
+    }
 };
 module.exports.userUpdate = function(req, res) { 
     sendJsonResponse(res, 200, {"status" : "success"})
