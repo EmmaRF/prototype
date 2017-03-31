6f97f65372ab047087a0b9311faba71dc7788edd
Maris Kanouff <mpicherkanouff@mills.edu>
2017-03-31 19:29:58 +0000

    models moved, json responses added

--- app_api/controllers/users.js
+++ app_api/controllers/users.js
@@ -1,4 +1,23 @@
-module.exports.user = function(req, res) { };
-module.exports.userCreate = function(req, res) { };
-module.exports.userUpdate = function(req, res) { };
-module.exports.userDelete = function(req, res) { };
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+
+var sendJsonResponse = function(res, status, content) {
+    res.status(status);
+    res.json(content);
+};
+
+module.exports.user = function(req, res) { 
+    sendJsonResponse(res, 200, {"status" : "success"})
+};
+module.exports.userCreate = function(req, res) { 
+    sendJsonResponse(res, 200, {"status" : "success"});
+};
+module.exports.userProfile = function(req, res) { 
+    sendJsonResponse(res, 200, {"status" : "success"})
+};
+module.exports.userUpdate = function(req, res) { 
+    sendJsonResponse(res, 200, {"status" : "success"})
+};
+module.exports.userDelete = function(req, res) { 
+    sendJsonResponse(res, 200, {"status" : "success"})
+};
