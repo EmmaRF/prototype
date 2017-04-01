0f8656160dfdb1d0a5189479a9ecac3db16b4101
kmanning <kmanning@mills.edu>
2017-04-01 01:47:20 +0000

    OK, now trying to go live with db ...

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
-var dbURI = 'mongodb://localhost/overbooked';
+// var dbURI = 'mongodb://localhost/overbooked';
+var dbURI = 'mongodb://kmanning@mills.edu:makestudios@ds147900.mlab.com:47900/heroku_htqtwj5x';
 mongoose.connect(dbURI);
 
 require('./userinfo');
