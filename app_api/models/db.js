8766dbb82c60b2eef13aead4bb895bfdd7b3cbdf
kmanning <kmanning@mills.edu>
2017-04-04 20:01:15 +0000

    changed dbURI to correct one

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var dbURI = 'mongodb://localhost/overbooked';
 if (process.env.NODE_ENV === 'production')
 {
-    var dbURI = 'mongodb://laghee:make17@ds147900.mlab.com:47900/heroku_htqtwj5x';
+    var dbURI = 'mongodb://heroku_htqtwj5x:ofi4732thkpivhc2fr8oi8dkff@ds147900.mlab.com:47900/heroku_htqtwj5x';
 }
 mongoose.connect(dbURI);
 
