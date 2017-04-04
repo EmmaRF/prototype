b1da277ab2510ff170ef0b625c23fc189a5d3aa1
kmanning <kmanning@mills.edu>
2017-04-04 19:58:47 +0000

    enabled production uri

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,6 +1,10 @@
 var mongoose = require('mongoose');
 var dbURI = 'mongodb://localhost/overbooked';
-// var dbURI = 'mongodb://laghee:make17@ds147900.mlab.com:47900/heroku_htqtwj5x';
+if (process.env.NODE_ENV === 'production')
+{
+    var dbURI = 'mongodb://laghee:make17@ds147900.mlab.com:47900/heroku_htqtwj5x';
+}
 mongoose.connect(dbURI);
 
 require('./userinfo');
+//hi
